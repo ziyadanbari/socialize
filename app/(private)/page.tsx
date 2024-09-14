42484e62ddfc445fa0efa3dd9dc2943efd9eea9b
ziyad <ziyadanbari@gmail.com>
2024-09-14 03:12:20 +0100

    remove toast popup in  home page

--- app/(private)/page.tsx
+++ app/(private)/page.tsx
@@ -1,16 +1,9 @@
 'use client'
 import { Button } from '@/components/ui/button'
-import { useToast } from '@/hooks/use-toast'
 import { signOut } from 'next-auth/react'
-import React, { useEffect } from 'react'
+import React from 'react'
 
 const Home = () => {
-  const {toast} = useToast()
-  useEffect(() => {
-    toast({
-      title: "test",
-    })
-  },[])
   return (
     <div>
       <Button variant={"destructive"} onClick={() => {
